e6fb4b852e354cfe15187b5e3e192d8882652b0d
Michael Christenson II <me@xorsense.nl>
2021-10-12 11:45:11 -0400

    Derive based copy

--- simple/simple.rs
+++ simple/simple.rs
@@ -1,5 +1,25 @@
+#[derive(Debug, Copy, Clone)]
+struct StrCopy {
+    value: &'static str
+}
+
+impl StrCopy {
+    fn new(v: &'static str) -> Self {
+        Self {
+            value: v
+        }
+    }
+}
+
 fn main() {
-    let prefix = "Hello, world!";
-    println!("{} from a macro", prefix);
-    println!("{} part deux", prefix);
+    let num = 42;
+    let prefix = StrCopy::new("Hello, world!");
+    preprint(prefix.into(), "this is Great!", num);
+    println!("{:?} from a macro {}", prefix, num);
+    println!("{:?} part deux {}", prefix, num);
+    preprint(prefix.into(), "this is bad!", num);
+}
+
+fn preprint(pre: StrCopy, msg: &str, num: i32) {
+    println!("{:?} {} {}", pre, msg, num)
 }
