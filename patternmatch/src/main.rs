c11b030e35fd796add504604ade728ab35bf1e23
David Fuka <gravetotkd@gmail.com>
2021-11-05 08:27:52 -0700

    We are confused

--- patternmatch/src/main.rs
+++ patternmatch/src/main.rs
@@ -1,20 +1,24 @@
-#[derive(Debug,Copy, Clone)]
+#[derive(Debug, Copy, Clone)]
 enum EyeColor {
     Blue,
     Black,
     Red,
-    None
+    None,
 }
 
 fn main() {
     let mut a = EyeColor::None;
-    print_eye_color(a);
+    //let mut b = "blue";
+    //print_eye_color(a);
     select_eye_color(&mut a, EyeColor::Blue);
+    println!("Selected EyeColor: {:?}", a);
+    print_eye_color(a);
+    select_eye_color_two(a, EyeColor::Red);
     print_eye_color(a);
 }
 
 fn print_eye_color(color: EyeColor) {
-    println!("Selected EyeColor: {:?}", color);
+    //println!("Selected EyeColor: {:?}", color);
     let value = match color {
         EyeColor::None => "Vacant",
         EyeColor::Black => "Deep",
@@ -26,4 +30,9 @@ fn print_eye_color(color: EyeColor) {
 
 fn select_eye_color(current_color: &mut EyeColor, select: EyeColor) {
     *current_color = select;
+}
+
+fn select_eye_color_two(mut current_color_two: EyeColor, select: EyeColor) {
+    current_color_two = select;
+    println!("I feel like {:?}", current_color_two)
 }
