35b024550152888b53b950b7bddb09ab26a8e687
David Fuka <gravetotkd@gmail.com>
2021-11-10 08:36:45 -0700

    more weird stuff

--- patternmatch/src/main.rs
+++ patternmatch/src/main.rs
@@ -8,12 +8,13 @@ enum EyeColor {
 
 fn main() {
     let mut a = EyeColor::None;
+    //let mut b = EyeColor::Black;
     //let mut b = "blue";
     //print_eye_color(a);
     select_eye_color(&mut a, EyeColor::Blue);
     println!("Selected EyeColor: {:?}", a);
     print_eye_color(a);
-    select_eye_color_two(a, EyeColor::Red);
+    // select_eye_color_two(&mut a, &mut b);
     print_eye_color(a);
 }
 
@@ -32,8 +33,7 @@ fn select_eye_color(current_color: &mut EyeColor, select: EyeColor) {
     *current_color = select;
 }
 
-fn select_eye_color_two(mut current_color_two: EyeColor, select: EyeColor) {
-    current_color_two = select;
-    println!("I feel like {:?}", current_color_two)
-}
-
+// fn select_eye_color_two(current_color_two: &'static mut EyeColor, select: &'static mut EyeColor) {
+//     current_color_two = select;
+//     //println!("I feel like {:?}", current_color_two)
+// }
