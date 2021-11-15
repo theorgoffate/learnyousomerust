ad592718488729f9dd8814a3ac7de3b7492c5ef2
Michael Christenson II <me@xorsense.nl>
2021-11-15 11:25:50 -0500

    Added hellowebapi

---
+++ hellohttpapi/src/main.rs
@@ -0,0 +1,24 @@
+#[macro_use] extern crate rocket;
+// For reference: https://rocket.rs/v0.5-rc/guide/requests/
+
+#[get("/introduce/<name>")]
+fn introduce(name: &str) -> String {
+    format!("Welcome, {}!", name)
+}
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .mount(
+            "/",
+            routes![
+                index,
+                introduce
+            ]
+        )
+}
